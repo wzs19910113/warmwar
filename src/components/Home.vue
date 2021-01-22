3dfc031467b791d9426d1d492404b5396fdeafda
Yuchen <wzs19910113@hotmail.com>
2021-01-23 07:26:43 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data(){
+            let msg = 'Blank Page 3'
+            return {
+                msg
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
